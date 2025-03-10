7764cf15c4dd6c55b21a181acc24fe2822ee1963
Pom <kasiphit@gmail.com>
2025-03-10 14:27:52 +0700

    feat:complete react components assignment

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="logo">🚀 XYZ CORP</div>
+        <ul className="nav-list">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
